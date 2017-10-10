5dfa234d2919359c0a69836a13463efb3c46cbad
Surakarn Samkaew <tonkla@gmail.com>
2017-10-10 22:26:46 +0700

    Complete all basic functions

--- src/list.js
+++ src/list.js
@@ -6,9 +6,10 @@ const moment = require('moment')
 Object.assign(String.prototype, {
   toFloat (opt = {}) {
     try {
-      const num = parseFloat(this.replace(/,/g, ''))
-      if (opt.multiply) return num * opt.multiply
-      else return num
+      const str = this.replace(/,/g, '')
+      if (str === '-') return 0
+      const num = parseFloat(str)
+      return opt.multiply ? num * opt.multiply : num
     } catch (err) {
       return this.toString()
     }
@@ -31,19 +32,19 @@ function get (symbol = '') {
       if (symbol) {
         switch (symbol.toUpperCase()) {
           case 'SET50':
-            output = getSET50()
+            output = getByIndex('SET50')
             break
           case 'SET100':
-            output = getSET100()
+            output = getByIndex('SET100')
             break
           case 'SSET':
-            output = getSSET()
+            output = getByIndex('sSET')
             break
           case 'SETHD':
-            output = getSETHD()
+            output = getByIndex('SETHD')
             break
           default:
-            output = get(symbol)
+            output = getBySymbol(symbol)
         }
       } else {
         output = getIndexes()
@@ -67,37 +68,86 @@ function getUpdatedAt ($) {
   }
 }
 
+function getUpdatedAtStr (str) {
+  try {
+    const text = str.split(' Last Update ')[1]
+    return moment(`${text} +0700`, 'DD MMM YYYY HH:mm:ss ZZ').toDate()
+  } catch (err) {
+    console.error(err)
+    return ''
+  }
+}
+
+function extractIndexes ($) {
+  const rows = $('#maincontent table.table-info tbody').eq(0).find('tr')
+  const indexes = []
+  rows.each((_, row) => {
+    const cols = $(row).find('td')
+    const symbol = cols.eq(0).text().trim()
+    const last = cols.eq(1).text().trim()
+    const change = cols.eq(2).text().trim()
+    const changePercent = cols.eq(3).text().trim()
+    const high = cols.eq(4).text().trim()
+    const low = cols.eq(5).text().trim()
+    const volume = cols.eq(6).text().trim()
+    const value = cols.eq(7).text().trim()
+    indexes.push({
+      symbol: symbol,
+      last: last.toFloat(),
+      change: change.toFloat(),
+      changePercent: changePercent.toFloat(),
+      high: high.toFloat(),
+      low: low.toFloat(),
+      volume: volume.toFloat({ multiply: 1e3 }),
+      value: value.toFloat({ multiply: 1e6 })
+    })
+  })
+  return indexes
+}
+
+function extractList ($) {
+  const rows = $('#maincontent table.table-info tbody').eq(2).find('tr')
+  const list = []
+  rows.each((_, row) => {
+    const cols = $(row).find('td')
+    const symbol = cols.eq(0).text().trim()
+    const sign = cols.eq(1).text().trim()
+    const open = cols.eq(2).text().trim()
+    const high = cols.eq(3).text().trim()
+    const low = cols.eq(4).text().trim()
+    const last = cols.eq(5).text().trim()
+    const change = cols.eq(6).text().trim()
+    const percentChange = cols.eq(7).text().trim()
+    const bid = cols.eq(8).text().trim()
+    const offer = cols.eq(9).text().trim()
+    const volume = cols.eq(10).text().trim()
+    const value = cols.eq(11).text().trim()
+    list.push({
+      symbol: symbol,
+      sign: sign,
+      open: open.toFloat(),
+      high: high.toFloat(),
+      low: low.toFloat(),
+      last: last.toFloat(),
+      change: change.toFloat(),
+      percentChange: percentChange.toFloat(),
+      bid: bid.toFloat(),
+      offer: offer.toFloat(),
+      volume: volume.toFloat(),
+      value: value.toFloat({ multiply: 1e3 })
+    })
+  })
+  return list
+}
+
 function getIndexes () {
   return new Promise((resolve, reject) => {
     try {
-      const url = 'https://marketdata.set.or.th/mkt/sectorquotation.do?sector=SET50&language=en&country=US'
+      const url = 'https://marketdata.set.or.th/mkt/marketsummary.do?language=en&country=US'
       request(url, (err, response, body) => {
         if (err) throw err
         const $ = cheerio.load(body)
-        const rows = $('#maincontent table.table-info tbody').eq(0).children('tr')
-        const indexes = []
-        rows.each((_, row) => {
-          const cols = $(row).find('td')
-          const symbol = cols.eq(0).text().trim()
-          const last = cols.eq(1).text().trim()
-          const change = cols.eq(2).text().trim()
-          const changePercent = cols.eq(3).text().trim()
-          const high = cols.eq(4).text().trim()
-          const low = cols.eq(5).text().trim()
-          const volume = cols.eq(6).text().trim()
-          const value = cols.eq(7).text().trim()
-          indexes.push({
-            symbol: symbol,
-            last: last.toFloat(),
-            change: change.toFloat(),
-            changePercent: changePercent.toFloat(),
-            high: high.toFloat(),
-            low: low.toFloat(),
-            volume: volume.toFloat({ multiply: 1e3 }),
-            value: value.toFloat({ multiply: 1e6 })
-          })
-        })
-        resolve({ updatedAt: getUpdatedAt($), indexes: indexes })
+        resolve({ updatedAt: getUpdatedAt($), indexes: extractIndexes($) })
       })
     } catch (err) {
       reject(err)
@@ -105,48 +155,92 @@ function getIndexes () {
   })
 }
 
-function getSET50 ($) {
-  try {
-    const body = ''
-    const $ = cheerio.load(body)
-    return ''
-  } catch (err) {
-    console.error(err)
-    return 0
-  }
+function getByIndex (index) {
+  return new Promise((resolve, reject) => {
+    try {
+      const url = `https://marketdata.set.or.th/mkt/sectorquotation.do?sector=${index}&language=en&country=US`
+      request(url, (err, response, body) => {
+        if (err) throw err
+        const $ = cheerio.load(body)
+        resolve({ updatedAt: getUpdatedAt($), indexes: extractIndexes($), stocks: extractList($) })
+      })
+    } catch (err) {
+      reject(err)
+    }
+  })
 }
 
-function getSET100 ($) {
-  try {
-    const body = ''
-    const $ = cheerio.load(body)
-    return ''
-  } catch (err) {
-    console.error(err)
-    return 0
-  }
-}
+function getBySymbol (symbol) {
+  return new Promise((resolve, reject) => {
+    try {
+      const url = `https://marketdata.set.or.th/mkt/stockquotation.do?symbol=${symbol.toUpperCase()}&ssoPageId=1&language=en&country=US`
+      request(url, (err, response, body) => {
+        if (err) throw err
+        const $ = cheerio.load(body)
+        const tables = $('#maincontent .row .row').eq(2).find('table')
 
-function getSSET ($) {
-  try {
-    const body = ''
-    const $ = cheerio.load(body)
-    return ''
-  } catch (err) {
-    console.error(err)
-    return 0
-  }
-}
+        let r = $(tables).eq(0).find('tr')
+        const updatedAt = r.eq(0).children().eq(0).text().trim()
+        const marketStatus = r.eq(1).children().eq(0).text().trim()
+        const sign = r.eq(2).children().eq(1).text().trim()
+        const last = r.eq(3).children().eq(1).text().trim()
+        const change = r.eq(4).children().eq(1).text().trim()
+        const percentChange = r.eq(5).children().eq(1).text().trim()
+        const prior = r.eq(6).children().eq(1).text().trim()
+        const open = r.eq(7).children().eq(1).text().trim()
+        const high = r.eq(8).children().eq(1).text().trim()
+        const low = r.eq(9).children().eq(1).text().trim()
+        const volume = r.eq(10).children().eq(1).text().trim()
+        const value = r.eq(11).children().eq(1).text().trim()
+        const average = r.eq(12).children().eq(1).text().trim()
+        let s = {
+          updatedAt: getUpdatedAtStr(updatedAt),
+          marketStatus: marketStatus.split(' : ')[1],
+          sign: sign,
+          last: last.toFloat(),
+          change: change.toFloat(),
+          percentChange: percentChange.toFloat(),
+          prior: prior.toFloat(),
+          open: open.toFloat(),
+          high: high.toFloat(),
+          low: low.toFloat(),
+          volume: volume.toFloat(),
+          value: value.toFloat({ multiply: 1e3 }),
+          average: average.toFloat()
+        }
+        const stock = { symbol: symbol.toUpperCase() }
+        Object.assign(stock, s)
 
-function getSETHD ($) {
-  try {
-    const body = ''
-    const $ = cheerio.load(body)
-    return ''
-  } catch (err) {
-    console.error(err)
-    return 0
-  }
+        r = $(tables).eq(1).find('tr')
+        const par = r.eq(0).children().eq(1).text().trim()
+        const ceiling = r.eq(1).children().eq(1).text().trim()
+        const floor = r.eq(2).children().eq(1).text().trim()
+        s = {
+          par: par.toFloat(),
+          ceiling: ceiling.toFloat(),
+          floor: floor.toFloat()
+        }
+        Object.assign(stock, s)
+
+        r = $(tables).eq(2).find('tr')
+        const b = r.eq(0).children().eq(1).text().trim()
+        const o = r.eq(1).children().eq(1).text().trim()
+        const bid = b.replace(/\s+/g, '').split('/')
+        const offer = o.replace(/\s+/g, '').split('/')
+        s = {
+          bid: bid[0].toFloat(),
+          bidVolume: bid[1].toFloat(),
+          offer: offer[0].toFloat(),
+          offerVolume: offer[1].toFloat()
+        }
+        Object.assign(stock, s)
+
+        resolve(stock)
+      })
+    } catch (err) {
+      reject(err)
+    }
+  })
 }
 
 module.exports = { get }
